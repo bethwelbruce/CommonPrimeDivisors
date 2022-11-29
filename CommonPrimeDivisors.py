e04f9228a8e2d8f9bc60cc2921f5239c919e3281
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2022-11-29 08:57:22 +0300

    Add files via upload

---
+++ CommonPrimeDivisors.py
@@ -0,0 +1,21 @@
+#euclidean Algorithm
+def gcd(a,b):
+    if a%b==0:
+        return b
+    else:
+        return gcd(b,a%b)
+def solution(A,B):
+    l=len(A)
+    cnt=0
+    for i in range(0,1):
+        a=A[i]
+        b=B[i]
+        D=gcd(a,b)
+        while(gcd(a,D)!=1):
+            a/=gcd(a,D)
+        while(gcd(b,D)!=1):
+            b/=gcd(b,D)
+        if(a==1 and b==1):
+            cnt+=1
+    return cnt
+    pass
